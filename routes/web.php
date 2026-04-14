c8751857ed9d62dc511fcd480411670aa10e8b80
Simon Griman <rayoysombra@gmail.com>
2026-04-14 11:50:16 -0400

    login: corregir doble mensaje de error y entrada directa al login

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
